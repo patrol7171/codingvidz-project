00c563d1ece8cdcbbe45423167ff375650b6c36b
patrol7171 <patrol7171@gmail.com>
2019-06-01 20:24:23 -0400

    model and views updated

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    youtube_id = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
--- groups/views.py
+++ groups/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from .models import Group, Video
+#from .forms import VideoForm, SearchForm
+from django.http import Http404, JsonResponse
+from django.forms.utils import ErrorList
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import urllib
+import requests
+
 
 def home (request):
     return render(request, 'groups/home.html')
+
+@login_required
+def dashboard(request):
+    groups = Group.objects.filter(user=request.user)
+    return render(request, 'groups/dashboard.html', {'groups':groups})
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('dashboard')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
