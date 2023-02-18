b71300eaff613cd1e5d7cb88ecdf5fa82c0c1cd9
devpatrol1 <patrol7171@gmail.com>
2023-02-18 15:14:53 -0600

    Updated static url ref

--- codingvidz/settings.py
+++ codingvidz/settings.py
@@ -126,7 +126,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
