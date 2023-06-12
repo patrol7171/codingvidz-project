7836de7d35bbac725c72c9721fcb4f29d1aacea7
devpatrol1 <patrol7171@gmail.com>
2023-06-12 16:33:19 -0500

    Update settings.py
    Changed dotenv reference

--- codingvidz/settings.py
+++ codingvidz/settings.py
@@ -12,17 +12,20 @@ import dotenv
 # Environment paths 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Use .env locally only
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    API_KEY = os.environ.get('API_KEY')
-else: # For deployment server only
-    dotenv.load_dotenv()
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    API_KEY = os.environ.get('API_KEY')
+# # Use .env locally only
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#     dotenv.load_dotenv(dotenv_file)
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#     API_KEY = os.environ.get('API_KEY')
+# else: # For deployment server only
+#     dotenv.load_dotenv()
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+#     API_KEY = os.environ.get('API_KEY')
 
+dotenv.load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
+API_KEY = os.environ.get('API_KEY')
 
 DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
