0f8048fc5f200b76e8ae7b39fd55c9cc0e87cbe6
devpatrol1 <patrol7171@gmail.com>
2023-06-28 21:49:43 -0500

    Update settings.py

--- codingvidz/settings.py
+++ codingvidz/settings.py
@@ -16,8 +16,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
-# else: # For deployment server only
-#     dotenv.load_dotenv()
+else: # For deployment server only
+    dotenv.load_dotenv()
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
