8dc7e67fcfb05a757035fc5b4a581b3a87bb5a0c
devpatrol1 <patrol7171@gmail.com>
2023-07-06 18:41:27 -0500

    Update settings.py
    - Re-added whitenoise settings

--- codingvidz/settings.py
+++ codingvidz/settings.py
@@ -131,9 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# WHITENOISE_USE_FINDERS = True
-# WHITENOISE_MANIFEST_STRICT = False
-# WHITENOISE_ALLOW_ALL_ORIGINS = True
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 # Activate Django-Heroku
 django_heroku.settings(locals())
